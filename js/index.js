b5c85004cade250dbed48af05b9e18c25b01d54a
Pavan Kumar Reddy Vuyyuru <pavansaivuyyyuru314@gmail.com>
2023-12-13 02:29:55 -0500

    Refactored js code for all pages

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+function toggleNavbar() {
+    const navbarLinks = document.querySelector('.navbar-links');
+    navbarLinks.style.display = navbarLinks.style.display === 'flex' ? 'none' : 'flex';
+}
 const registeredUser = window.localStorage.getItem("exampleLogin__firstname");
 window.addEventListener('DOMContentLoaded', displayName)
 
