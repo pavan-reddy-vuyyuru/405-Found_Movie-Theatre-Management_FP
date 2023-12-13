c46e5769009199830a2f2a0b52ea24e3cd568517
Pavan Kumar Reddy Vuyyuru <pavansaivuyyyuru314@gmail.com>
2023-12-13 04:34:04 -0500

    Refactored the code and mobile specific changes.

--- js/index.js
+++ js/index.js
@@ -1,12 +1,24 @@
-function toggleNavbar() {
-    const navbarLinks = document.querySelector('.navbar-links');
-    navbarLinks.style.display = navbarLinks.style.display === 'flex' ? 'none' : 'flex';
-}
-const registeredUser = window.localStorage.getItem("exampleLogin__firstname");
+registeredUser = window.localStorage.getItem("exampleLogin__firstname");
 window.addEventListener('DOMContentLoaded', displayName)
+
+if (!registeredUser) {
+    window.location.href = 'invalid-login.html';
+}
 
 function displayName() {
     var usernameDisplay = document.getElementById('usernameDisplay');
     usernameDisplay.innerHTML = `${registeredUser}`;
 
 };
+
+function toggleNavbar() {
+    const navbarLinks = document.querySelector('.navbar-links');
+    navbarLinks.style.display = navbarLinks.style.display === 'flex' ? 'none' : 'flex';
+}
+document.getElementById('mainContainer').addEventListener('touchend', function() {
+    touchEndFunc();
+});
+
+function touchEndFunc() {
+    alert("Your touch ended");
+}
